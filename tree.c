9738bdbe52c8cc2b3ff8f8080557b1e6726986c9
Sandbom <Simon.sandbom@gmail.com>
2015-10-19 01:22:47 +0200

    Update tree.c

--- tree.c
+++ tree.c
@@ -5,10 +5,8 @@
 
 
 #include "items.h"
+#include "tree.h"
 
-
-
-//Trädets nodes / branches uppbyggnad //
 
 typedef struct treenode {
   item_t *item;
@@ -16,16 +14,11 @@ typedef struct treenode {
   struct treenode *right;
 } treenode_t;
 
-
-//Trädets uppbygnad //
-
 typedef struct tree{
   treenode_t *root;
 } tree_t;
 
-/// Creates a new tree
-///
-/// \returns: empty tree
+
 tree_t *tree_new(){
   tree_t *emptytree = NULL;
   emptytree = calloc(1, sizeof(tree_t));
@@ -33,9 +26,6 @@ tree_t *tree_new(){
 }
 
 
-/// Get the size of the tree 
-///
-/// \returns: the number of nodes in the tree
 int tree_size(treenode_t *tree)
   {
     int i = 1;
@@ -48,8 +38,6 @@ int tree_size(treenode_t *tree)
     }
   }     
 
-
-/// Scans left path from node for smallest value (since smallest will always be on left side)
 treenode_t *minimum_Node(treenode_t *treenode){
 
   while (treenode->left != NULL){
@@ -57,22 +45,6 @@ treenode_t *minimum_Node(treenode_t *treenode){
   }
   return treenode;
 }
-
-
-
-// Get the depth of the tree 
-//
-// \returns: the depth of the deepest subtree
-/*int tree_depth(tree_t *tree){
-  if (tree == NULL) return 0;
-  else {
-
-
-  }
-}
-*/
-///Calculates the maxdepth of the tree
-///i.e the number of nodes from the root to the farthest leaf
 
 int max_depth(treenode_t *tree){
   int leftDepth;
@@ -89,7 +61,6 @@ int max_depth(treenode_t *tree){
     }
 }
 
-// return pointer to node with data //
 treenode_t* find_node(treenode_t *treenode, char *name){
   if(treenode == NULL){
     return NULL;
@@ -105,9 +76,6 @@ treenode_t* find_node(treenode_t *treenode, char *name){
       }
 }
   
-
-
-/// Finds and returns the inorder successor in bst // 
 treenode_t* inorder_successor(treenode_t *treenode, char *name){
   treenode_t *current  = find_node(treenode, name);
   if (current == NULL){
@@ -133,9 +101,6 @@ treenode_t* inorder_successor(treenode_t *treenode, char *name){
    return successor;
      }
 }
-
-
-
 
 
 treenode_t* tree_remove(treenode_t **treenode, char *name){
@@ -204,7 +169,7 @@ treenode_t* tree_insert(treenode_t **treenode, item_t *item){
 
   else if(strcmp(itemname(item), itemname((*treenode)->item)) == 0)
     (*treenode)->left= tree_insert(&(*treenode)->left , item);
-  // Some repetition here, could probably easily be fixed.
+
     
     else (*treenode)->right = tree_insert(&(*treenode)->right, item);
   
@@ -220,88 +185,3 @@ void ordered_print(treenode_t *treenode){
   printf("%s", itemname(treenode->item));
   ordered_print(treenode->right);
 }
-						  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main(){
-  printf("Hello world");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/// FUNCTIONS THAT NEED LOVE ///
-
- /// Deleting a node from the tree
-
-//struct treenode_t* tree_remove(struct treenode_t *root, char *name)
-//{
-  /* (FUNGERAR EJ, behöver fixas stort)
-  if (*treenode == NULL) return 0;
-
-  else if(name < warename((*root->item))) root->left = tree_remove(root->left, item_t);
-  else if(name > warename((*root->item))) root->right = tree_remove(root->right, item_t);
-  else
-    {
-      /// Node has no children
-      if (root->left == NULL && root->right == NULL){
-	free(treenode);
-      }
-      /// Node has 1 left child
-      else if (root->left == NULL){
-	struct treenode *temp = root;
-	root = root -> left;
-	free(temp);
-
-      }
-      /// Node has 1 right child
-      else if(root->right == NULL) {
-	struct treenode *temp = root;
-	root = root->right;
-	free(temp);
-      }
-      else{
-	struct treenode *temp = minNode(root->right);
-	root->name = temp->name;
-	root->right = tree_remove(root->right, temp->name);
-      }
-    }
-}
-return treenode;
-	
-  */
