57204c8ce81eff7130ff9cfb04d7bde1d87d6bce
Eric Falheim <ericfalheim@hotmail.com>
2015-10-26 08:03:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,46 @@
-// main.c
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 
-
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  return 0;
+  bool should_quit = false;
+  
+  while (!should_quit)
+    {
+      puts("AREPLUQ"); // Valmöjligheter, ersätt med print main menu() eller något
+      char input = getchar(); // getchar är förenklat, ersätt med get char när moduler sätts in
+      
+      switch(toupper(input))
+	{
+	case 'A': // Add
+	  puts("A"); 
+	  break;
+	  
+	case 'R': // Remove
+	  puts("R");
+	  break;
+	  
+	case 'E': // Edit
+	  puts("E");
+	  break;
+	  
+	case 'L': // List
+	  puts("L");
+	  break;
+	  
+	case 'U': // Undo
+	  puts("U");
+	  break;
+	  
+	case 'Q': // Quit
+	  should_quit = true;
+	  break;
+	  
+	default:
+	  puts("Something went wrong. err: default case in main");
+	}
+    }
 }
